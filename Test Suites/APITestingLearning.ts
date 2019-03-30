18e9db26634cef3a5f217a35644f63805283f4c9
angee <angeetong@gmail.com>
2019-03-30 15:31:10 -0700

    first project commit

---
+++ Test Suites/APITestingLearning.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>APITestingLearning</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>fc4c6565-630f-45e8-a4a4-6a252cec887e</testSuiteGuid>
+   <testCaseLink>
+      <guid>ad600018-bdad-4705-bf5c-17c963dd29f7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/HTTPMethods</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
